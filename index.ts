ad978110299186e693bea044c469275f21c60c8d
domen <domen.sapac10@gmail.com>
2026-05-02 19:01:29 +0200

    Connected Supabase + storing refNumbers in database

--- index.ts
+++ index.ts
@@ -1,31 +1,77 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { chromium } from 'playwright';
+import { createClient } from '@supabase/supabase-js'
 
-try{
-    let options = {
-    headers: {
-        'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'
-    }
-    }
-    let url = "https://www.nepremicnine.net/oglasi-oddaja/podravska/stanovanje/cena-do-500-eur-na-mesec/"; 
-    const response = await axios.get(url, options);
-    
-    const $ = cheerio.load(response.data); 
+console.log("Prebran URL:", process.env.SUPABASE_URL);
 
-    /*
-    $("div.property-details h2, div.property-details>h6").each((index, element) => {
-        //console.log($(element).text() + "\n"); 
-    })
-    */
-    $("div.property-box").each((index, element) => {
-        //console.log($('meta[itemprop="mainEntityOfPage"]').first().attr('content') + "\n") ;
-        let elementUrl = $('meta[itemprop="mainEntityOfPage"]').first().attr('content'); 
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseKey = process.env.SUPABASE_ANON_KEY!
+export const supabase = createClient(supabaseUrl, supabaseKey); 
+
+
+
+async function scrapeData(){
+    var mainUrl = "https://www.nepremicnine.net/oglasi-oddaja/podravska/stanovanje/cena-do-500-eur-na-mesec/"; 
+    var options = {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1',
+            'Accept': 'application/json'
+        }}
+
+    try{
+        const wholeData = await axios.get(mainUrl, options); 
+        const $ = cheerio.load(wholeData.data); 
+
+        let elementUrls : string[] = []; 
+
+        $("div.property-box").each((index, element) => {
+            let elementUrl = $(element).find('meta[itemprop="mainEntityOfPage"]').attr('content'); 
+            //console.log(element)
+            if(elementUrl)
+                elementUrls.push(elementUrl);
+        })
+
+        let responses = []; 
+        for(let url of elementUrls){
+             const result = await axios.get(url, options); 
+             responses.push(result.data); 
+        }
         
-    })
+        let referenceNumbers = []; 
+        for(let element of responses){
+            const $ = cheerio.load(element); 
+            let referenceNumber = $("div.desc-box").find('strong.fs-15').text(); 
+            referenceNumbers.push(referenceNumber); 
+        }
+    
+        printRefNumbers(referenceNumbers); 
+        await insertData(referenceNumbers); 
+        
+        const { data, error } = await supabase.from('my_table').select('*'); 
+        console.log("Data", data); 
+        console.log("Error", error);
 
-}catch (error){
-    console.error("Error: ", error.message); 
+        
+        
+    }catch(error){
+        console.log("Error:", error); 
+    }
 }
 
 
+scrapeData();
 
+function printRefNumbers(data : any[]){
+    for(let element of data){
+        console.log(element);
+    }
+}
+
+async function insertData(data : any[]){
+    for(let element of data){
+        const { error } = await supabase
+        .from('my_table')
+        .insert({ ref_number : element}); 
+    }
+}
