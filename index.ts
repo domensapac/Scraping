0018d00361d3de2c1227ca56b76f2032963f1a13
domen <domen.sapac10@gmail.com>
2026-05-05 17:16:00 +0200

    Trying proxy

--- index.ts
+++ index.ts
@@ -23,8 +23,12 @@ async function scrapeData(){
     var mainUrl = "https://www.nepremicnine.net/oglasi-oddaja/podravska/maribor/mb-center,maribor,za-kalvarijo,koroska-vrata/stanovanje/1-sobno,15-sobno,2-sobno,garsonjera/cena-do-500-eur-na-mesec/?nadst%5B0%5D=vsa&nadst%5B1%5D=vsa"; 
     console.log("Obiskana glavna stran."); 
 
-    const browser = await chromium.launch({ headless: true });
-
+    const browser = await chromium.launch({
+        headless: true,
+        proxy: {
+            server: '31.15.169.77:808' // Poišči "Slovenia free proxy list"
+        }
+    });
     const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         viewport: { width: 1280, height: 720 }
@@ -36,7 +40,7 @@ async function scrapeData(){
         await page.goto(mainUrl, { waitUntil: 'load', timeout: 60000 });
         const title = await page.title();
         console.log("Naslov strani:", title);
-         
+
         await page.waitForTimeout(5000);
         const wholeData = await page.content();
         const $ = cheerio.load(wholeData); 
