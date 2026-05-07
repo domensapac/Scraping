c5093cc006bb9d513b93020be392b983c8a0ea1f
domen <domen.sapac10@gmail.com>
2026-05-07 23:04:14 +0200

    Revert "Added another user"
    This reverts commit 9d5a2397418a588010908f1ebecb76a09a885c66.

--- index.ts
+++ index.ts
@@ -171,7 +171,7 @@ async function sendMail(data : any[]){
 
     const mailOptions = {
         from: '"Nepremičnine.net" <domen.sapac420@gmail.com>',
-        to: 'domen.sapac10@gmail.com, potocnik.126@gmail.com',
+        to: 'domen.sapac10@gmail.com',
         subject: 'Najdeni novi oglasi',
         html : htmlString
     }
